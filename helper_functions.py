5fb5ed56bd875a2a9e4085f7e91c20cd32d4c987
bwozniak <bwozniak@umich.edu>
2021-12-29 17:33:57 -0500

    slimmed deducer

--- helper_functions.py
+++ helper_functions.py
@@ -150,29 +150,26 @@ class Minesweeper:
         return coords
     
     def deducer(self, unopened, num_bombs, other_squares):
-        decoding_dict = {}
         to_flag = []
         to_click = []
         restart = False
         for neighbor in other_squares:
             unopened2, bombs2, other_squares2 = self.get_surrounding_tiles(neighbor[0], neighbor[1])
             num_bombs2 = self.board[neighbor[0]][neighbor[1]] - len(bombs2)
-            decoding_dict[neighbor] = [num_bombs2, unopened2]
-        for square in decoding_dict:
-            if decoding_dict[square][0] == 0:
-                for box in decoding_dict[square][1]:
+            if num_bombs2 == 0:
+                for box in unopened2:
                     self.board[box[0]][box[1]] = 0
                     # TODO: change back to + 1
                     to_click.append((box[0] + 1, box[1] + 1))
                 
-                self.completed[square[0]][square[1]] = True
-            elif decoding_dict[square][0] == len(decoding_dict[square][1]):
-                for box in decoding_dict[square][1]:
+                self.completed[neighbor[0]][neighbor[1]] = True
+            elif num_bombs2 == len(unopened2):
+                for box in unopened2:
                     self.board[box[0]][box[1]] = -2
                     # TODO: change back to + 1
                     to_flag.append((box[0] + 1, box[1] + 1))
                 
-                self.completed[square[0]][square[1]] = True
+                self.completed[neighbor[0]][neighbor[1]] = True
             else:
                 # try all combos of bombs for current,
                 # check if any work with neighbors
@@ -184,12 +181,12 @@ class Minesweeper:
                 remaining_tiles = []
                 for combo in bomb_combos:
                     a = set(combo) # current square
-                    b = set(decoding_dict[square][1]) # neighbor square
+                    b = set(unopened2) # neighbor square
                     intersect = a.intersection(b)
-                    if len(intersect) > decoding_dict[square][0]:
+                    if len(intersect) > num_bombs2:
                         # invalid combo
                         bomb_combos.remove(combo)
-                    if len(intersect) == decoding_dict[square][0]:
+                    if len(intersect) == num_bombs2:
                         # valid combo
                         remaining = b - a
                         remaining_tiles.append(remaining)
